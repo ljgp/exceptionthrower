67de9e0e69bb157db8a0b143d50ec5a9bb2e0ecf
ljgp <lpeerdeman@gmail.com>
2022-11-11 18:35:44 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,23 @@
 package org.example;
- 
-public class Main {  
-  public static void main(String[] args) {
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world");
-         
-         System.out.println("Entering recursion...");
-         doeiets();
-   
-   final ServerSocket server = new ServerSocket(8080);
-         System.out.println("Listening for connection on port 8080 ....");
-         while (true){
-           // spin forever
-         }
+
+        System.out.println("Entering recursion...");
+        doeiets();
+
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true){
+            // spin forever
+        }
     }
 
     private static void doeiets() {
         doeiets();
     }
 } 
-
