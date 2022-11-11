eb395b6a5c9ee6eabf7cdc063488839bec74c224
ljgp <lpeerdeman@gmail.com>
2022-11-11 18:09:50 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,10 @@ public class Main {
         System.out.println("Hello world!\nHello universe\nhello multiverse\n\n\n\n\nhello multiline\n");
         (new Throwable("oeps")).printStackTrace();
         System.out.println("bye");
+        doeiets();
+    }
+    
+    private static void doeiets() {
+        throw new IllegalArgumentException("foute boel");
     }
 }
